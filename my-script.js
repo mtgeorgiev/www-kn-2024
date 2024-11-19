f240e6bcad975221fdcfc662e026b5f6f1448d57
Mihail Georgiev <mihail.georgiev@brandwatch.com>
2024-11-19 20:01:50 +0200

    some javascript, register form

--- my-script.js
+++ my-script.js
@@ -15,6 +15,65 @@ for (let i = 0; i < 20; i ++) {
 
 document.body.appendChild(container);
 
+const showRegisterForm = () => {
+  document.getElementById("register-form")
+          .classList.remove("hidden");
+};
+
+const hideRegisterForm = () => {
+  document.getElementById("register-form")
+          .classList.add("hidden");
+};
+
+const displayRegisterError = (message) => {
+  let errorElement = document.createElement('div');
+  errorElement.innerText = message;
+  errorElement.setAttribute('id', 'register-error');
+  errorElement.classList.add('errors');
+
+  document.getElementById('register-form').appendChild(errorElement);
+
+
+  // let errorElement = document.getElementById("register-error");
+  // errorElement.innerText = message;
+  // errorElement.classList.remove("hidden");
+};
+
+document.getElementById('show-register-form')
+        .addEventListener('click', showRegisterForm);
+
+document.getElementById('hide-register-form')
+        .addEventListener('click', hideRegisterForm);
+
+document.getElementById('register-form')
+        .addEventListener('submit', event => {
+          event.preventDefault();
+
+          for (let element of event.target.getElementsByClassName('errors')) {
+            element.parentElement.removeChild(element);
+          }
+
+          let password = event.target.querySelector('input[name="password"').value;
+          let confirmPassword = event.target.querySelector('input[name="confirm-password"').value;
+
+          if (password !== confirmPassword) {
+            displayRegisterError('Passwords do not match');
+          } else {
+            let body = new FormData(); // new FormData(event.target)
+            body.append('username', event.target.querySelector('input[name="username"').value);
+            body.append('password', password);
+
+
+            fetch('./register.php', {
+              method: 'POST',
+              body: body,
+            });
+          }
+        });
+
+
+
+
 class MishoElement extends HTMLElement {
     constructor() {
       super();
